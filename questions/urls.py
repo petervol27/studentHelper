9ff3312452587a804b6cc74cbb0fb00fa99fd23b
yaarySh <yaary1526@gmail.com>
2025-02-17 22:45:02 +0200

    added submit url

--- questions/urls.py
+++ questions/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("add/", views.generate_questions, name="add_question"),
     path("list/", views.list_question, name="list_question"),
+    path("submit_answer", views.submit_code, name="submit_answer"),
 ]
